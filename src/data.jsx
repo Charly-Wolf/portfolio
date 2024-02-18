4f6093549ae1c8878a325a19951ee9922ea36e2a
Charly <cardan1990@hotmail.com>
2024-02-18 19:15:36 +0100

    Deleted Home from nav links

--- src/data.jsx
+++ src/data.jsx
@@ -7,7 +7,7 @@ import imgMixMaster from './assets/01 - MixMaster.png'
 import imgUnsplash from './assets/02 - Unsplash.png'
 
 export const links = [
-  { id: nanoid(), href: '#home', text: 'home' },
+  // { id: nanoid(), href: '#home', text: 'home' },
   { id: nanoid(), href: '#skills', text: 'skills' },
   { id: nanoid(), href: '#about', text: 'about' },
   { id: nanoid(), href: '#projects', text: 'projects' },
